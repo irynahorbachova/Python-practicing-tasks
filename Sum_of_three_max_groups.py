ae927d38644de751cbe3fe543dbb177e33c9edb9
Iryna Horbachova <66974494+irynahorbachova@users.noreply.github.com>
2021-09-22 03:17:15 +0300

    Create Sum_of_three_max_groups.py

---
+++ Sum_of_three_max_groups.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df=pd.DataFrame({
+    'grps':list('aaabbcaabcccbbc'), 
+    'vals':[12,345,3,1,45,14,4,52,54,23,235,21,57,3,87]})
+
+result = df.groupby('grps')['vals'].nlargest(3).sum(level=0)
+print(result)
